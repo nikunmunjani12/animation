180dcbb729766abfafd64414c2f9b0eeeb441fae
Destiny <nikunjmunjani814@gmail.com>
2023-06-24 10:19:18 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
-import 'package:animation/3-Color-tween_animation.dart';
 import 'package:flutter/material.dart';
-import '1-Animation-Container.dart';
-import '2-Animation-Controller.dart';
-import '4-BorderRadiusTween-Animation.dart';
-import '5-BorderTween-Animation.dart';
 import '6-All-Color-Animation.dart';
-import '7-TextStyle-Tween-Animation.dart';
-import 'Demo.dart';
-import 'Lottei-Package-Animation.dart';
 
 void main() {
   runApp(const MyApp());
